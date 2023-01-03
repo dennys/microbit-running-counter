3823fb4a15e96dbb2f2fd827739042d217b2ccf1
Dennys Hsieh <dennys@users.noreply.github.com>
2023-01-03 10:25:17 +0800

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-def on_gesture_shake():
-    global 步數
-    步數 += 1
-input.on_gesture(Gesture.SHAKE, on_gesture_shake)
-
 def on_gesture_screen_up():
-    basic.show_number(步數)
+    basic.show_number(Step_Count)
 input.on_gesture(Gesture.SCREEN_UP, on_gesture_screen_up)
 
 def on_button_pressed_ab():
-    global 步數
-    步數 = 0
+    global Step_Count
+    Step_Count = 0
     basic.clear_screen()
     basic.show_icon(IconNames.NO)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
-步數 = 0
-步數 = 0
+def on_gesture_shake():
+    global Step_Count
+    Step_Count += 1
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+Step_Count = 0
+Step_Count = 0
--- main.ts
+++ main.ts
@@ -1,12 +1,14 @@
-input.onGesture(Gesture.ScreenUp, function () {
+input.onGesture(Gesture.ScreenUp, function on_gesture_screen_up() {
     basic.showNumber(Step_Count)
 })
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    
     Step_Count = 0
     basic.clearScreen()
     basic.showIcon(IconNames.No)
 })
-input.onGesture(Gesture.Shake, function () {
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    
     Step_Count += 1
 })
 let Step_Count = 0
